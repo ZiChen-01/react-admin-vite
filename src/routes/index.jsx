46a8733f2984f85b87a1e419288cb57cbef4c346
ZiChen-Jiang <18307106535@163.com>
2023-05-09 10:53:49 +0800

    修复路由菜单icon

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,13 @@
-import Icon, { UserOutlined, HomeOutlined } from '@ant-design/icons';
+import { UserOutlined, HomeOutlined } from '@ant-design/icons';
 // 使用路由懒加载
 import React, { lazy } from "react";
 import getMenu from "@/routes/routerConfig";
 //用于获取状态
 import store from "@/stores/store";
+
+//引入antd-icon
+import * as Icon from '@ant-design/icons';
+// 基础菜单
 let routes = [
     {
         label: "首页",
@@ -17,16 +21,50 @@ let routes = [
         key: "/system",
         element: lazy(() => import("@/pages/system")),
         icon: <UserOutlined />,
-        children: [],
+        children: [
+            {
+                label: "用户管理",
+                key: "/isystem/user",
+                element: lazy(() => import("@/pages/isystem/user")),
+                icon: null,
+                disabled: false,
+            },
+            {
+                label: "角色管理",
+                key: "/isystem/roleUserList",
+                element: lazy(() => import("@/pages/isystem/roleUserList")),
+                icon: null,
+                disabled: false,
+            },
+            {
+                label: "菜单管理",
+                key: "/isystem/permission",
+                element: lazy(() => import("@/pages/isystem/permission")),
+                icon: null,
+                disabled: false,
+            },
+            {
+                label: "机构管理",
+                key: "/isystem/deptNew",
+                element: lazy(() => import("@/pages/isystem/deptNew")),
+                icon: null,
+                disabled: false,
+            },
+        ],
         disabled: false,
     },
 ]
 
+
+// 登录后刷新页面
 store.subscribe(() => {
     const { reload } = store.getState()
     if (reload) location.reload()
 })
 
+//创建节点的方法
+function iconBC(name) { return React.createElement(Icon[name]); }
+// 从接口请求菜单
 if (localStorage.getItem('Autn-Token')) {
     routes = await getMenu()
     function setIcon(res) {
@@ -34,7 +72,7 @@ if (localStorage.getItem('Autn-Token')) {
             const element = res[i];
             let IconItem = element.icon
             // element.icon = < Icon component={IconItem} />
-            element.icon = <HomeOutlined />
+            if (IconItem) element.icon = iconBC(IconItem)
             if (element.children) setIcon(element.children)
         }
     }
