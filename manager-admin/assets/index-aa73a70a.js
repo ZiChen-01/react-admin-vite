3ab8c55383781b7838f1ab5a01cf3427529b0c6b
ZiChen-Jiang <18307106535@163.com>
2023-05-25 16:02:38 +0800

    1

---
+++ manager-admin/assets/index-aa73a70a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-7736204d.js";function e(){return r.jsx(r.Fragment,{children:"监控数据"})}export{e as default};
---
+++ manager-admin/assets/index-e51aa039.js
@@ -0,0 +1,1 @@
+import{r as e,j as s}from"./index-7736204d.js";const n=""+new URL("welcome-49086098.png",import.meta.url).href;class a extends e.Component{render(){return window.envConfig.ROOT_APP_NAME,s.jsx("main",{className:"analsis",children:s.jsxs("div",{className:"imgbox",children:[" ",s.jsx("img",{src:n,alt:""})]})})}}export{a as default};
