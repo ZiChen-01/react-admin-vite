3107950a2488d7c1d669660164da8f8e2e19a608
YiMing-Jiang <18307106535@163.com>
2023-08-23 14:49:17 +0800

    1

--- src/pages/problem/index.jsx
+++
@@ -1,8 +0,0 @@
-function Problem() {
-    return(
-        <>
-        监控数据
-        </>
-    )
-}
-export default Problem
--- src/pages/statistics/index.jsx
+++
@@ -1,9 +0,0 @@
-function Statistics() {
-    return (
-        <>
-            统计管理
-        </>
-    )
-}
-
-export default Statistics
