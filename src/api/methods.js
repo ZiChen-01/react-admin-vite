fc7236b68b0a4225206a0dd4fb4833e5853dc753
YiMing-Jiang <18307106535@163.com>
2023-09-26 10:09:47 +0800

    总有一天，我们会在相遇！

--- src/api/methods.js
+++ src/api/methods.js
@@ -1,8 +1,10 @@
 import Server from '@/utils/request'
 import { notification } from 'antd'
+let API_BASE_PORT = window.envConfig['API_BASE_PORT']
 // 封装请求方式
 // ----------------methods:请求方式   url:请求地址   params：请求参数----------------
 export function axios(methods, url, params) {
+	url = url.includes(API_BASE_PORT) ? url : API_BASE_PORT + url
 	switch (methods) {
 		case 'get':
 			return Server({
@@ -46,8 +48,8 @@ export function axios(methods, url, params) {
 		case "download"://下载文件
 			return Server({
 				url: url,
-				method: 'get',
-				params,
+				method: 'post',
+				data: params,
 				responseType: 'blob'
 			});
 		default:
